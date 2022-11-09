9f4992cb4666bd2fc8a03d63bb02692096cd7b6b
Amir Reza Abbasi <amir.reza2823@gmail.com>
2022-11-09 11:26:07 +0330

    make customer model

---
+++ src/main/java/com/chaapaar/demo/model/Customer.java
@@ -0,0 +1,29 @@
+package com.chaapaar.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 100)
+    private String firstName;
+    @Column(length = 100)
+    private String lastName;
+    @Column(length = 500)
+    private String description;
+    @Column(length = 100, nullable = false)
+    private String email;
+}
